063b0a73f69dc49b990b1ff421f48a86c1c7a30c
pitcher0303 <pitcher0303@gmail.com>
2018-04-04 10:08:47 +0900

    Client.java add main, startClient

--- Client/Client.java
+++ Client/Client.java
@@ -1,1 +1,11 @@
 //클라이언트 프로그램 작성
+
+Class Client {
+	public void startClient() {};
+
+	public static void main(String[] args) {
+		//Start Client
+		Client client = new Client();
+		client.startClient();
+	}
+}
